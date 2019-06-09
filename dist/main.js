f7c9ce984904471aa16296778e4d9346135cd09c
Nimpo <cloud_nimpo@ukr.net>
2019-06-10 01:45:59 +0300

    added functionality

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,11 @@
 function GetRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+function ChangeBackgroundColor(element, color) {
+  element.style.backgroundColor = color;
+}
+
 //Timer
 let counter = 60;
 function TimeIt() {
@@ -14,7 +19,6 @@ function TimeIt() {
 let interval = setInterval(TimeIt, 1000);
 
 //Display Multiplication
-//let isRightAnswer;
 let currRightAnswer;
 function DisplayMultiplication() {
   let num1 = GetRandomNumber(1, 9);
@@ -24,24 +28,74 @@ function DisplayMultiplication() {
   currRightAnswer = num1 * num2;
 }
 DisplayMultiplication();
-console.log(currRightAnswer);
 
 //Display AnswersVariants
+let answers = document.getElementsByClassName("answer");
+
 function DisplayAnswers() {
-  let answers = Array.prototype.slice.call(
-    document.getElementsByClassName("answer"),
-    0
-  );
-  let randomIndex = GetRandomNumber(0, answers.length);
-  console.log(randomIndex);
-  for (let i = 0; i < answers.length; i++) {
+  let answersToArray = Array.prototype.slice.call(answers, 0);
+  let randomIndex = GetRandomNumber(0, answers.length - 1);
+
+  for (let i = 0; i < answersToArray.length; i++) {
+    //Need to make not repeat the numbers
     if (i === randomIndex) {
-      answers[i].innerHTML = currRightAnswer;
+      answersToArray[i].innerHTML = currRightAnswer;
     } else {
-      answers[i].innerHTML = GetRandomNumber(1, 81);
+      if (currRightAnswer > 5) {
+        answersToArray[i].innerHTML = GetRandomNumber(
+          currRightAnswer - 5,
+          currRightAnswer + 5
+        );
+      } else {
+        answersToArray[i].innerHTML = GetRandomNumber(1, currRightAnswer + 5);
+      }
     }
   }
 }
 DisplayAnswers();
 
-//Click Answer
+//Do GameLogic
+for (let i = 0; i < answers.length; i++) {
+  answers[i].addEventListener("click", DoGameLogic);
+}
+let choosenAnswer;
+function DoGameLogic(e) {
+  let potentialAnswer = e.target;
+  choosenAnswer = potentialAnswer;
+  if (parseInt(potentialAnswer.innerHTML) === currRightAnswer) {
+    ChangeBackgroundColor(potentialAnswer, "green");
+    AddPoints();
+    setTimeout(LoadNextLvl, 500);
+  } else {
+    ChangeBackgroundColor(potentialAnswer, "red");
+    MinusPoints();
+    setTimeout(TryAgain, 250);
+  }
+  e.preventDefault();
+}
+
+function LoadNextLvl() {
+  choosenAnswer.style.backgroundColor = "";
+  DisplayMultiplication();
+  DisplayAnswers();
+}
+
+function TryAgain() {
+  choosenAnswer.style.backgroundColor = "";
+}
+
+//Regulate Points
+let countPoints = 0;
+let points = document.getElementById("value3");
+
+function AddPoints() {
+  countPoints += 100;
+  points.innerHTML = countPoints;
+}
+
+function MinusPoints() {
+  if (countPoints >= 100) {
+    countPoints -= 100;
+    points.innerHTML = countPoints;
+  }
+}
